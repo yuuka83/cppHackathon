9004ffd7f0263eff655b2d6bb8cf535bf6099e5f
yuuka83 <s2210017@jaist.ac.jp>
2024-02-26 19:56:47 +0900

    ボックスコライダーの定義

--- src/Components/ColliderComponent.cpp
+++ src/Components/ColliderComponent.cpp
@@ -2,20 +2,24 @@
 #include "../Actors/Actor.h"
 #include <iostream>
 
+// コンストラクタ
+ColliderComponent::ColliderComponent(Actor *owner, float width, float height)
+    : Component(owner, 10), mWidth(width), mHeight(height)
+{
+}
+// 中心位置を返す
 const Vector2 &ColliderComponent::GetCenterPosition() const
 {
+    return mOwner->GetPosition();
 }
 
-// 衝突し始めを検知
-bool ColliderComponent::OnCollisionEnter(ColliderComponent &a, ColliderComponent &b)
+bool ColliderComponent::IsColliding(ColliderComponent &a, ColliderComponent &b)
 {
-}
-// 衝突している時を検知
-bool ColliderComponent::OnCollisionStay(ColliderComponent &a, ColliderComponent &b)
-{
-}
-
-// 衝突し終わった時を検知
-bool ColliderComponent::OnCollisionExit(ColliderComponent &a, ColliderComponent &b)
-{
+    // x軸とy軸で重なりがあるかチェック
+    // aの左線の座標がbの左線の座標よりも小さいかつaの右線の座標がbの右線の座標よりも大きい
+    bool overlapX = a.GetCenterPosition().x - a.mWidth / 2 < b.GetCenterPosition().x - b.mWidth / 2 &&
+                    a.GetCenterPosition().x + a.mWidth / 2 > b.GetCenterPosition().x + b.mWidth / 2;
+    bool overlapY = a.GetCenterPosition().y - a.mHeight / 2 < b.GetCenterPosition().y - b.mHeight / 2 &&
+                    a.GetCenterPosition().y + a.mHeight / 2 > b.GetCenterPosition().y + b.mHeight / 2;
+    return overlapX && overlapY;
 }
--- src/Components/ColliderComponent.h
+++ src/Components/ColliderComponent.h
@@ -1,16 +1,14 @@
 #include "Component.h"
 #include "../Math.h"
-// 円コライダー
+// ボックスコライダー
 class ColliderComponent : public Component
 {
 public:
-    ColliderComponent(class Actor *owner);
+    ColliderComponent(Actor *owner, float width, float height);
     ~ColliderComponent();
     const Vector2 &GetCenterPosition() const;
-    virtual bool OnCollisionEnter(ColliderComponent &a, ColliderComponent &b);
-    virtual bool OnCollisionStay(ColliderComponent &a, ColliderComponent &b);
-    virtual bool OnCollisionExit(ColliderComponent &a, ColliderComponent &b);
+    bool IsColliding(ColliderComponent &a, ColliderComponent &b);
 
 private:
-    bool mCollisionEnter;
+    float mWidth, mHeight;
 };
